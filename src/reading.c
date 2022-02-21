7c537317ce43779db86a39161dbc4627088696a8
Nicolas maria Ortolan soto <nortolan@c1r15s3.42madrid.com>
2022-02-21 19:13:57 +0100

    i think it's working, need to test a lot

--- src/reading.c
+++ src/reading.c
@@ -6,15 +6,15 @@
 /*   By: nortolan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 12:06:09 by nortolan          #+#    #+#             */
-/*   Updated: 2022/02/18 18:03:53 by nortolan         ###   ########.fr       */
+/*   Updated: 2022/02/21 19:12:57 by nortolan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-//TODO: test fallido: tabulaciones;
-//TODO: test fallido: a"b'c'd"e
-//TODO: test fallido: "b'c'd"e
+//TODO: que cojones:  | "|asdsdfh"| ó  | "|asdsdfh"|
+//TODO: tabulaciones arregladas?;
+//TODO: probar si renta que a'b'c'd'e sea un solo token;
 //TODO: testear a full;
 //TODO: mirar leaks;
 
@@ -32,18 +32,26 @@ void	get_lines(char *line)
 	aux_count = 0;
 	while (line[i] || status == 1)
 	{
+		//printf("char actual: %c\n", line[i]);
 		if (status == 2 || status == 4)
 			status = 0;
 		if (status == 0)
 			count = 0;
-		if (line[i + 1] == '\0' && status == 1 && (line[i] != '\"' && line[i] != '\''))
+		if ((line[i] == '\0' && status == 1) || (line[i + 1] == '\0' && status == 1 && ((line[i] != '\"' && line[i] != '\'') || aux_count > 0)))
 		{
+			//printf("klk aux_count: %d\n", aux_count);
 			write (2, "Quotation marks not closed\n", 27);
 			break;
 		}
+		if (i == 0)
+		{
+			while (line[i] == ' ' || line[i] == '\t')
+				i++;
+		}
 		if (line[i] != '|')
 		{
-			while (line[i] != '|' && line[i] != ' ' && line[i] != '\"' && line[i] != '\'' && line[i])
+			//printf("HoLAAAA\n");
+			while (line[i] != '|' && line[i] != ' ' && line[i] != '\t' && line[i] != '\"' && line[i] != '\'' && line[i])
 			{
 				count++;
 				i++;
@@ -63,16 +71,17 @@ void	get_lines(char *line)
 				//printf("status: %d\n\n", status);
 				if (line[i] == line[i - count - 1]/* ||(line[i - count - 1] != '\"' && line[i - count - 1] != '\'')*/)
 				{
-					status = 4;
+					status = 2;
 					i++;
 				}
 				else
 				{
-					//printf("estoy aqui lol\n");
+					//printf("estoy aqui lol %c\n", line[i]);
 					//TODO: cambiar esto por un else if;
 					if (line[i - count - 1] != '\"' && line[i - count - 1] != '\'')
 					{
 						aux_count = i - count - 1;
+						//printf("aqui tambien XD\n");
 						while (++aux_count < i && aux_count >= 0)
 						{
 							//printf("char: %c\n", line[aux_count]);
@@ -81,12 +90,16 @@ void	get_lines(char *line)
 								if (line[aux_count] == line[i])
 								{
 									//printf("he entrado aqui\n");
-									status = 4;
-									//i++;
+									status = 2;
+									i++;
 									aux_count = -2;
 								}
 								else
+								{
+									i++;
+									count++;
 									aux_count = -2;
+								}
 							}
 						}
 						if (aux_count != -2)
@@ -99,13 +112,13 @@ void	get_lines(char *line)
 					{
 						i++;
 						count++;
+						aux_count = -2;
 					}
 					/*printf("ALGO VA MAL\n");
 					break ;*/
 				}
 			}
-			//printf("status: %d\n", status);
-			if (status == 2 && line[i] != ' ' && line[i] != '|' && line[i])
+			if (status == 2 && line[i] != ' ' && line[i] != '\t' && line[i] != '|' && line[i])
 			{
 				status = 3;
 			}
@@ -118,8 +131,8 @@ void	get_lines(char *line)
 					token->data = ft_substr(line, i - count, count);
 				if (status == 2 || status == 4)
 					token->data = ft_substr(line, i - count - 2, count + 2);
-				if (status == 4)
-					token->data = ft_substr(line, i - count - 2, count + 2);
+				/*if (status == 4)
+					token->data = ft_substr(line, i - count - 2, count + 2);*/
 				token->type = 1;
 				token->next = NULL;
 				if (i - count == 0 || (status == 2 && i - count - 2 == 0))
@@ -152,7 +165,7 @@ void	get_lines(char *line)
 				count++;
 			i++;
 		}
-		while (line[i] == ' ')
+		while (line[i] == ' ' || line[i] == '\t')
 		{
 			count++;
 			i++;
