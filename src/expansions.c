b80b39caf5bf68cb2a0856ab6237f54cf3c65358
Nicolas maria Ortolan soto <nortolan@c1r15s1.42madrid.com>
2022-03-17 14:10:35 +0100

    working on expansions

--- src/expansions.c
+++ src/expansions.c
@@ -6,7 +6,7 @@
 /*   By: nortolan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 11:22:34 by nortolan          #+#    #+#             */
-/*   Updated: 2022/03/17 13:22:41 by nortolan         ###   ########.fr       */
+/*   Updated: 2022/03/17 14:10:10 by nortolan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,27 @@
 //TODO: la variable pilla todo lo que sean numeros y letras;
 //TODO: si va entre comillas, to los caracteres palante;
 //TODO: fuera de comillas se lia con el &, el > o <, el ( o ), el \, el ';';
-/*void	get_variable(t_reading *vars, int i)
-{
+//TODO: si la variable no existe y no estamos en comillas simples, se reemplaza por nada;
+//TODO; tests importantes con variables que no existen: "'$aaa'"/"$aa'a'a"/ "$aaa"/'$aaa'
+//TODO: un dollar suelto se imprime tal cual
 
-}i*/
+void	get_variable(t_reading *vars, int i)
+{
+	int		l;
+	char	*name;
+
+	l = 0;
+	name = NULL;
+	while ((vars->token->data[i] >= 65 && vars->token->data[i] <= 90) || (vars->token->data[i] >= 97 && vars->token->data[i] <= 122) || (vars->token->data[i] >= 48 && vars->token->data[i] <= 57))
+	{
+		l++;
+		printf("CHAR: %c\n", vars->token->data[i]);
+		i++;
+	}
+	printf("length: %d\n", l);
+	//TODO: tener en cuenta si lenght = 0 porque es un dollar suelto;
+	//TODO: malloc aqui al name para copiarlo y conseguir luego el value
+}
 
 void	tok_expand(t_reading *vars)
 {
@@ -63,7 +80,7 @@ void	tok_expand(t_reading *vars)
 			{
 				printf("FUNCIONA\n");
 				i++;
-				//get_variable(vars, i);
+				get_variable(vars, i);
 			}
 			else
 				i++;
