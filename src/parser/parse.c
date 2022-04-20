67983a971058a9af68c18d8f8427ad6668c86471
Nicolas maria Ortolan soto <nortolan@c2r16s4.42madrid.com>
2022-04-20 21:07:22 +0200

    leaks fixed

--- src/parser/parse.c
+++ src/parser/parse.c
@@ -6,7 +6,7 @@
 /*   By: nortolan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 18:44:27 by nortolan          #+#    #+#             */
-/*   Updated: 2022/04/20 20:33:25 by nortolan         ###   ########.fr       */
+/*   Updated: 2022/04/20 21:06:25 by nortolan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,34 @@ t_cmd	*last_cmd(t_cmd *lst)
 	return (lst);
 }
 
+void	free_args(char **argv)
+{
+	int	i;
+
+	i = -1;
+	while (argv[++i])
+		free(argv[i]);
+	free(argv);
+}
+
+void	cmds_clear(t_parse *parse)
+{
+	t_cmd	*temp;
+
+	temp = NULL;
+	parse->cmds = parse->head_cmd;
+	if (parse->cmds == NULL)
+		return ;
+	printf("funciono!\n");
+	while (parse->cmds)
+	{
+		free_args(parse->cmds->argv);
+		temp = parse->cmds;
+		parse->cmds = parse->cmds->next;
+		free(temp);
+	}
+}
+
 void	create_cmd(t_reading *vars, t_parse *parse)
 {
 	int		i;
@@ -44,7 +72,7 @@ void	create_cmd(t_reading *vars, t_parse *parse)
 		temp_cmd = parse->cmds;
 		parse->cmds = parse->cmds->next;
 	}
-	parse->cmds = malloc(sizeof(t_parse));
+	parse->cmds = malloc(sizeof(t_cmd));
 	if (parse->cmds == NULL)
 		exit (1);
 	if (temp_cmd)
@@ -52,6 +80,8 @@ void	create_cmd(t_reading *vars, t_parse *parse)
 	parse->cmds->argv = malloc(sizeof(char *) * (parse->wc + 1));
 	if (parse->cmds->argv == NULL)
 		exit (1);
+	if (temp_cmd == NULL)
+		parse->head_cmd = parse->cmds;
 	while (++i < parse->wc_aux)
 	{
 		temp_token = temp_token->next;
@@ -69,7 +99,10 @@ void	create_cmd(t_reading *vars, t_parse *parse)
 	////////////TESTING//////////////
 	i = -1;
 	while (++i < parse->wc)
+	{
 		printf("cmds: %s\n", parse->cmds->argv[i]);
+//		printf("cmds: %p\n", parse->cmds->argv[i]);
+	}
 	printf("cmds: %s\n", parse->cmds->argv[i]);
 	printf(">>>>>>>>>>>><<<<<<<<<<<<<\n");
 	///////////TESTING//////////////
@@ -79,7 +112,6 @@ void	create_cmd(t_reading *vars, t_parse *parse)
 void	get_cmd(t_reading *vars, t_parse *parse)
 {
 	vars->token = vars->head;
-	parse->head_cmd = parse->cmds;
 	while (vars->token)
 	{
 		if (vars->token->type == 2 || (vars->token->next == NULL && vars->token->type != 2 && ++parse->wc))
@@ -110,4 +142,5 @@ void	parse(t_reading *vars)
 	/////////////////////////////////////////////
 	parse_init(&parse);
 	get_cmd(vars, &parse);
+	cmds_clear(&parse);
 }
