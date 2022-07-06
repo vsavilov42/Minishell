ab5b9d6307de19694595ef5d62068bb20b8091fe
Daniel Exposito lopez <dexposit@c1r9s3.42madrid.com>
2022-07-06 15:03:10 +0200

    create all fork needed and start initialize_exec_struct()

--- src/exec/executer.c
+++ src/exec/executer.c
@@ -6,7 +6,7 @@
 /*   By: dexposit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 13:23:23 by dexposit          #+#    #+#             */
-/*   Updated: 2022/07/05 19:01:03 by dexposit         ###   ########.fr       */
+/*   Updated: 2022/07/06 14:58:04 by dexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,30 +27,51 @@
 void	executer(t_parse *cmd)
 {
 	t_cmd	*aux;
+	pid_t	id;
+	int		status;
 
 	aux = cmd->head_cmd;
-	while (aux)
+	id = create_process(aux, NULL);
+/*	while (aux)
 	{
 		//create_process(g_sh.cmd);
 		printf("%s\n", *(aux->argv));
 		aux = aux->next;
-	}
-	exit(0);
+	}*/
+	waitpid(id, &status, 0);
+//	exit(status);
 }
 
-void	create_process(t_cmd *cmd, t_exec *prev)
+pid_t	create_process(t_cmd *cmd, t_exec *prev)
 {
 	t_exec	*own;
 
-	if (!cmd || !prev)
-		return ;
 	own = (t_exec *) malloc(sizeof (t_exec *));
-	own->pid = fork();
+	//rellenar structura own
+	if (cmd->next)
+		own->pid = fork();
 	if (own->pid < 0)
 		perror("Fail to do fork\n");
-	else if (own->pid == 0)
-		//hijo volver a llamar crear proceso
+	else if (own->pid == 0 && cmd->next)
+		create_process(cmd->next, own);
+	else if (own->pid == 0 && !cmd->next)
+		printf("ultimo hijo: %s\n", *(cmd->argv));
+	else if (prev)
+		printf("process padre hijo  of the command: %s\n", *(cmd->argv));
 	else
-		//padre cambiar in out luego ejecutar
-	//	builtin(cmd->argv);
+		printf("1processo cmd %s\n", *(cmd->argv));
+//	if (prev)
+//		waitpid(prev->pid, &prev->status, 0);
+	return (own->pid);
+}
+
+void	initialize_exec_struct(t_cmd *cmd, t_exec *prev)
+{
+	t_exec	*res;
+
+	res = (t_exec *) malloc(sizeof(t_exec *));
+	if (!res)
+		perror("Fail to reserve memory\n");
+	if (cmd->next)
+		pipe(res->pipe_fd);
 }
