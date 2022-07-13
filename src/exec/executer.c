adcfb9f7b68025a25af2bfcebca0a452d16be4c7
Daniel Exposito lopez <dexposit@c1r6s6.42madrid.com>
2022-07-13 13:34:12 +0200

    add comment to explain functions logic

--- src/exec/executer.c
+++ src/exec/executer.c
@@ -6,7 +6,7 @@
 /*   By: dexposit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 13:23:23 by dexposit          #+#    #+#             */
-/*   Updated: 2022/07/12 20:28:08 by dexposit         ###   ########.fr       */
+/*   Updated: 2022/07/13 13:28:18 by dexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,9 +40,20 @@ void	executer(t_parse *cmd)
 	waitpid(g_sh.lst_id, &status, 0);
 //	exit(status);
 }
-
 /*
  * REVISAR COMO DEBEN ESPERARSE LOS PROCESOS
+ */
+/*
+ *	CREATE_PROCESS:
+ *
+ *	tiene que crear un proceso para cada comando de la estructura t_cmd
+ * 	luego de crear los pertinentes procesos se debe ejecutar cada comando en el proceso que corresponda
+ * 	antes de eso usaremos dup dependiendo que tipo de cmd sea
+ * 		1- para las pipes
+ * 		2- luego para las redirecciones < << > >>
+ * 		3- cerrar todos los fd no usados
+ * 		4- ejecutar el comando
+ * 			si no ejecuta: liberar todo y pertinentes status de salia
  */
 pid_t	create_process(t_cmd *cmd, t_exec *prev)
 {
@@ -56,8 +67,9 @@ pid_t	create_process(t_cmd *cmd, t_exec *prev)
 	else if (own->pid == 0 && !cmd->next)
 	{
 		g_sh.lst_id = own->pid;
-	//	execute_cmd(cmd);
+		//execute_cmd(cmd);
 	}
+
 		//dup entrada a pipe prev
 	//	printf("ultimo hijo: %s\n", *(cmd->argv));
 	else if (prev)
@@ -75,7 +87,7 @@ t_exec	*initialize_exec_struct(t_cmd *cmd)
 
 	res = (t_exec *) malloc(sizeof(t_exec));
 	if (!res)
-		return (perror("Fail to reserve memory\n"), NULL);
+		return (perror("Fail to reserve memory\n"), free(res), NULL);
 	if (cmd->next || (cmd->pos == 0))
 	{
 		pipe(res->pipe_fd);
@@ -98,13 +110,7 @@ int	execute_cmd(t_cmd *cmd)
 
 	if (!is_builtin(cmd->argv))
 		return (builtin(cmd->argv), 0);
-//	cmd->env = create_path(cmd);
 	cmd->env = env_pointer();
-/*	///////TEST CREATE_PATH//////
-	int i=-1;
-	while (cmd->env[++i])
-		printf("linea %d del env:\n%s\n", i, cmd->env[i]);
-*/
 //	printf("split_cmd\n");
 	split_cmd = save_cmd_with_arguments(cmd);
 //	printf("cmd???	%s\n", split_cmd[0]);
@@ -116,11 +122,12 @@ int	execute_cmd(t_cmd *cmd)
 //	printf("----------------------\n");
 //	printf("cmd path: %s\n", cmd->cmd_path);	
 //	execve(cmd->cmd_path, split_cmd, cmd->env);
-	return (1);
+	return (-1);
 }
 
 /*	
- *	fill_cmd_path
+ *	FILL_CMD_PATH:
+ *
  *	recortar la línea paht del env
  *	splitear esta línea por : sin la parte del inicio
  *	comprobar con access( ejecución) de cada ruta de nuestro enviroment junto al cmd pasado como argumento.
@@ -179,6 +186,11 @@ char	**create_path(t_cmd *cmd)
 	return (env);
 }
 
+/*	///////TEST CREATE_PATH//////
+	int i=-1;
+	while (cmd->env[++i])
+		printf("linea %d del env:\n%s\n", i, cmd->env[i]);
+*/
 void	command_path(t_cmd *cmd, char *path_line)
 {
 	char	**envtmp;
