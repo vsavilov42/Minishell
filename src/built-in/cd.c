a46d00c54a6a21c4708bdd1b2a95ad2abec4c446
Vsavilov <vsavilov@student.42madrid.com>
2022-07-19 17:14:13 +0200

    oldpwd && - done

--- src/built-in/cd.c
+++ src/built-in/cd.c
@@ -6,14 +6,15 @@
 /*   By: Vsavilov <Vsavilov@student.42Madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 11:36:16 by Vsavilov          #+#    #+#             */
-/*   Updated: 2022/07/18 23:13:21 by Vsavilov         ###   ########.fr       */
+/*   Updated: 2022/07/19 17:11:32 by Vsavilov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
 static void	ft_oldpwd(void);
-static void	ft_home(void);
+static void	cd_home(void);
+static void	cd_last(void);
 
 static void	ft_oldpwd(void)
 {
@@ -27,7 +28,7 @@ static void	ft_oldpwd(void)
 	{
 		if (!same_strcmp("OLDPWD", lst->name))
 		{
-			if (!getcwd(oldpwd, PATH_MAX))
+			if (getcwd(oldpwd, PATH_MAX))
 				lst->value = oldpwd;
 			return ;
 		}
@@ -37,7 +38,7 @@ static void	ft_oldpwd(void)
 	envlst_add_back(g_sh.env, new_envlst(tmp));
 }
 
-static void	ft_home(void)
+static void	cd_home(void)
 {
 	t_envlst	*lst;
 	char		*home;
@@ -52,28 +53,65 @@ static void	ft_home(void)
 	}
 	if (chdir(home))
 		write(2, "ShiTTYsh: cd: HOME not set\n", 27);
+	free(home);
 }
+
+static void	cd_last(void)
+{
+	t_envlst	*lst;
+
+	lst = *g_sh.env;
+	while (lst)
+	{
+		if (!same_strcmp("OLDPWD", lst->name))
+		{
+			if (chdir(lst->value))
+			{
+				write(2, "ShiTTYsh: cd: OLDPWD not set\n", 29);		
+			}
+			return ;
+		}
+		lst = lst->next;
+	}
+	write(2, "ShiTTYsh: cd: OLDPWD not set\n", 29);		
+}
+
+void	update_pwd()
+{
+	t_envlst	*lst;
+	char		*pwd;
+
+	pwd = (char *)malloc(sizeof(char) * (PATH_MAX + 1));
+	lst = *g_sh.env;
+	while (lst)
+	{
+		if (!same_strcmp("PWD", lst->name))
+		{
+			if (getcwd(pwd, PATH_MAX))
+				lst->value = ft_strdup(pwd);
+		}
+		lst = lst->next;
+	}
+	free(pwd);
+}
+
 int	ft_cd(char **arg)
 {
-	char	*tmp;
-
 	ft_oldpwd();
+	if (arg[1] && chdir(arg[1]) && same_strcmp(arg[1], "-"))
+	{
+		write(2, "ShiTTYsh: cd: ", 14);
+		write(2, arg[1], ft_strlen(arg[1]));
+		write(2, ": No such file or directory\n", 28);
+	}
 	if (!arg[1])
-		ft_home();
+		cd_home();
+	if (!same_strcmp(arg[1], "-"))
+		cd_last();
 	//comprobar si es cd solo
-	//sino ver si es - / ~
+	//sino ver si es - ~
 	//- regresa a oldowd
 	//sino carpeta origen
-	tmp = strtolow(arg[0]);
-	if (same_strcmp(tmp, "cd"))
-		return(0);
-	if (arg[1] != NULL)
-		if (chdir(arg[1]))
-		{
-			write(2, "ShiTTYsh: cd: ", 14);
-			write(2, arg[1], ft_strlen(arg[1]));
-			write(2, ": No such file or directory\n", 28);
-		}
-	free(tmp);
+	update_pwd();
 	return (1);
 }
