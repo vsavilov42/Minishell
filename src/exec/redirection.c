f2b7c82f677a4bd98a573d57f95e20140a9f9265
Daniel Exposito lopez <dexposit@c1r8s5.42madrid.com>
2022-07-20 14:38:32 +0200

    new .c for functions of redirections like < << > >> or |

---
+++ src/exec/redirection.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirection.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dexposit <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/20 13:46:15 by dexposit          #+#    #+#             */
+/*   Updated: 2022/07/20 14:09:57 by dexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+/*
+ * DUP_IN_OUT:
+ * Esta función realizara un dup de stdin por fin y stdout por fout
+ */
+int	dup_in_out(int *fin, int *fout, int type)
+{
+
+}
