3d46c9e207297722b072177618cff4d777264b54
Vsavilov <vsavilov@student.42madrid.com>
2022-09-30 15:13:13 +0200

    token_list in progress

--- src/ast/ast_token_list.c
+++ src/ast/ast_token_list.c
@@ -1,15 +1,28 @@
 #include <minishell.h>
 
+static t_ast	*token_list2(void)
+{
+	t_ast	*result;
+
+	result = redir();
+	if (!result)
+		return (NULL);
+	token_list();
+	astree_add_branch(g_sh.command, result, 0);
+	return (result);
+}
+
 static t_ast	*token_list1(void)
 {
 	char	*name;
 
-	name = NULL;
-	if (!(*ast)->data)
+	if (!g_sh.command->data)
 	{
-		astree_set_data(*ast, name);
-		token_list(ast);
-		return (*ast);
+		if (!ast_valid_char(&name, TOK_DEFAULT))
+			return (NULL);
+		astree_set_data(g_sh.command, name);
+		token_list();
+		return (g_sh.command);
 	}
 	return (NULL);
 }
@@ -22,6 +35,18 @@ t_ast	*token_list()
 	save = g_sh.tok;
 	g_sh.tok = save;
 	new_node = token_list1();
+	if (new_node)
+		return (new_node);
+	g_sh.tok = save;
+	new_node = token_list2();
+	if (new_node)
+		return (new_node);
+	g_sh.tok = save;
+	new_node = token_list3();
+	if (new_node)
+		return (new_node);
+	g_sh.tok = save;
+	new_node = token_list4();
 	if (new_node)
 		return (new_node);
 	return (NULL);
